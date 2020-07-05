21aa61e46aab1754cb2a8dc2f9fb447225d1ddfa
dev-calves <alves.c90@outlook.com>
2020-07-05 01:14:06 -0400

    route for checking refresh token

--- api/oauth/oauth.js
+++ api/oauth/oauth.js
@@ -11,6 +11,14 @@ router.get('/oauth/access', (req, res, next) => {
   })
 })
 
+router.get('/oauth/refresh', (req, res, next) => {
+  if (req.headers['x-refresh-token']) {
+    res.status(200).json({ 'refresh-token-available': true })
+  } else {
+    res.status(200).json({ 'refresh-token-available': false })
+  }
+})
+
 router.get('/oauth/delete', (req, res, next) => {
   // delete tokens.
   OAuthUtility.deleteTokens(req, res)
--- api/oauth/oauth.spec.js
+++ api/oauth/oauth.spec.js
@@ -22,7 +22,6 @@ describe('Oauth API', () => {
         jest.resetModules()
       })
 
-      // async errors
       test('should respond with an ok status when the tokens are retrieved', async (done) => {
         response.on('end', function () {
           const routeResponse = response._getJSONData()
@@ -35,6 +34,63 @@ describe('Oauth API', () => {
       })
     })
   })
+
+  describe('refresh route', () => {
+    describe('true response', () => {
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/oauth/refresh',
+        headers: {
+          'x-refresh-token': 'refresh1234'
+        }
+      })
+      const response = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      })
+
+      beforeEach(() => {
+        jest.resetModules()
+      })
+
+      test('should respond with a refresh status of true when the header exists', async (done) => {
+        response.on('end', function () {
+          const routeResponse = response._getJSONData()
+          expect(routeResponse).toBeDefined()
+          expect(routeResponse).toEqual({ 'refresh-token-available': true })
+          done()
+        })
+
+        routeHandler(request, response)
+      })
+    })
+
+    describe('false response', () => {
+      const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/oauth/refresh',
+        headers: {}
+      })
+      const response = httpMocks.createResponse({
+        eventEmitter: require('events').EventEmitter
+      })
+
+      beforeEach(() => {
+        jest.resetModules()
+      })
+
+      test('should respond with a refresh status of false when the header does not exist', async (done) => {
+        response.on('end', function () {
+          const routeResponse = response._getJSONData()
+          expect(routeResponse).toBeDefined()
+          expect(routeResponse).toEqual({ 'refresh-token-available': false })
+          done()
+        })
+
+        routeHandler(request, response)
+      })
+    })
+  })
+
   describe('delete route', () => {
     describe('successful response', () => {
       const request = httpMocks.createRequest({
@@ -49,7 +105,6 @@ describe('Oauth API', () => {
         jest.resetModules()
       })
 
-      // async errors
       test('should respond with an ok status when the tokens are deleted', async (done) => {
         response.on('end', function () {
           const routeResponse = response._getJSONData()
