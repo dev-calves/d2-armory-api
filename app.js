f4ccc87fdc4beb859cc7d88ca651fb660010fed2
dev-calves <alves.c90@outlook.com>
2020-07-16 02:24:17 -0400

    removed cors

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const express = require('express')
 const path = require('path')
 const logger = require('morgan')
 const helmet = require('helmet')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 // utilities
@@ -23,18 +22,10 @@ const oauthController = require('./api/oauth/oauth')
 // create express app
 const app = express()
 
-// cors configuration.
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
-  credentials: true, // added to allows storing cookies to the client
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 // mount middleware dependencies.
 app.use(logger(process.env.MORGAN_SETTING))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors(corsOptions))
 app.use(helmet())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
