b7a5dc29a8cdf22292798bfc2ce8f69943eb3fae
dev-calves <alves.c90@outlook.com>
2020-07-21 00:07:57 -0400

    close connection on access requests

--- api/oauth/oauth.js
+++ api/oauth/oauth.js
@@ -13,6 +13,8 @@ router.get('/oauth/access', (req, res, next) => {
 
     console.info('/oauth/access - response: ', message)
 
+    res.shouldKeepAlive = false // prevent duplicate requests with stale 'code'
+
     return res.status(200).json(message)
   }).catch(error => {
     next(error.response)
